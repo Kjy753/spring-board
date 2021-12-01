cfa609f217d46be7630242eb68f0b3496f46f992
kjy <aty753@naver.com>
2021-12-01 20:31:02 +0900

    BoardVO 작성 및 mapper 인터페이스및 xml 작성을 통해 출력 확인

---
+++ src/main/java/com/kjy/mapper/BoardMapper.java
@@ -0,0 +1,13 @@
+package com.kjy.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.kjy.domain.BoardVO;
+
+public interface BoardMapper {
+
+	//@Select("select * from sp_board where bno > 0")
+	public List<BoardVO> getList();
+}
