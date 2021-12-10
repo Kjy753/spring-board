65a43b726c62bc02544b3e6d204a24eda6521edf
kjy <aty753@naver.com>
2021-12-10 16:44:55 +0900

    댓글 서비스, 서비스impl 작성

---
+++ src/main/java/com/kjy/service/ReplyService.java
@@ -0,0 +1,24 @@
+package com.kjy.service;
+
+import java.util.List;
+
+import com.kjy.domain.Criteria;
+import com.kjy.domain.ReplyVO;
+
+
+public interface ReplyService {
+	
+	public int register(ReplyVO vo);
+	
+	public ReplyVO get(Long rno);
+	
+	public int modify(ReplyVO vo);
+	
+	public int remove(Long rno);
+	
+	//public List<BoardVO> getList();
+	
+	/* 검색의 기준인 cri 를 포함한 목록 조회로 변경 */
+	public List<ReplyVO> getList(Criteria cri, Long bno);
+
+}
