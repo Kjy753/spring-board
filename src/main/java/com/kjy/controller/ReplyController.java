fe9d4416e2593c0b9bdf311cc22e46076db8c7b6
kjy <aty753@naver.com>
2021-12-13 16:06:01 +0900

    댓글 목록을 조회 하는 작업

--- src/main/java/com/kjy/controller/ReplyController.java
+++ src/main/java/com/kjy/controller/ReplyController.java
@@ -1,18 +1,24 @@
 package com.kjy.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kjy.domain.Criteria;
 import com.kjy.domain.ReplyVO;
 import com.kjy.service.ReplyService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import oracle.net.ano.Service;
 
 @RequestMapping("/replies/")
 @RestController
@@ -20,18 +26,39 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class ReplyController {
 
-	private ReplyService serivice;
+	private ReplyService service;
 	
-	@PostMapping(value = "/new", consumes = "application/json", produces = { MediaType.TEXT_PLAIN_VALUE})
+	@PostMapping(value = "/new",
+			consumes = "application/json", 
+			produces = { MediaType.TEXT_PLAIN_VALUE})
 	public ResponseEntity<String> create(@RequestBody ReplyVO vo){
 		
 		log.info("ReplyVO : " + vo);
 		
-		int insertCount = serivice.register(vo);
+		int insertCount = service.register(vo);
 		
 		log.info("댓글 추가 : " + insertCount);
 		
 		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK): new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		
 	}
+	
+	@GetMapping(value = "/pages/{bno}/{page}",
+			produces = { MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(
+			@PathVariable("page") int page,
+			@PathVariable("bno") Long bno){
+		
+		log.info("get List............");
+		
+		Criteria cri = new Criteria(page,10);
+		
+		log.info("cri");
+		
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+		
+	}
+	
+	
 }
